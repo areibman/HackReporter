381f76abaede01ad1ccb9e5fcc65815744d8abf1
reibs <areibman@gmail.com>
2025-07-13 16:37:55 -0700

    stable tools commit final)

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,5 +1,6 @@
-from tools.gemini_video_tool import GeminiVideoTool
-from tools.twitter_tool import TwitterSearchTool
-from tools.typefully_tool import TypefullyTool, TypefullyScheduleTool
+from .gemini_video_tool import GeminiVideoTool
+from .twitter_tool import TwitterSearchTool
+from .typefully_tool import TypefullyTool
+from .file_reader_tool import FileReaderTool
 
-__all__ = ['GeminiVideoTool', 'TwitterSearchTool', 'TypefullyTool', 'TypefullyScheduleTool']
+__all__ = ['GeminiVideoTool', 'TwitterSearchTool', 'TypefullyTool', 'FileReaderTool']
